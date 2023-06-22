e73bc40994fd8b2d6581fe4b878f04b22e526967
Ilia Reingold <ilia.reingold@gmail.com>
2023-06-21 22:16:16 -0400

    fix bug

--- android/src/main/java/com/wallpaperai/wallpaperhelper/WallpaperHelperPlugin.java
+++ android/src/main/java/com/wallpaperai/wallpaperhelper/WallpaperHelperPlugin.java
@@ -12,23 +12,18 @@ public class WallpaperHelperPlugin extends Plugin {
     private WallpaperHelper implementation = new WallpaperHelper();
 
     @PluginMethod
-    public void echo(PluginCall call) {
-        String value = call.getString("value");
-
-        JSObject ret = new JSObject();
-        ret.put("value", implementation.echo(value));
-        call.resolve(ret);
-    }
-
-    @PluginMethod
     public void setWallpaper(PluginCall call) {
         String base64Image = call.getString("base64");
-        boolean setBoth = call.getBoolean("setBoth", false);
-
-        String result = implementation.setWallpaper(base64Image, setBoth);
+        boolean isLockScreen = call.getBoolean("isLockScreen", false); // default to false if not provided
 
         JSObject ret = new JSObject();
-        ret.put("result", result);
-        call.resolve(ret);
+
+        try {
+            String result = implementation.setWallpaper(base64Image, isLockScreen);
+            ret.put("value", result);
+            call.resolve(ret);
+        } catch (Exception e) {
+            call.reject(e.getMessage(), e);
+        }
     }
 }
