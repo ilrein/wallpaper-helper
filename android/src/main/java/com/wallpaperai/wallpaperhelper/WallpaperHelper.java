9c06db9cf036ecef77ed033b28c398f0d1ae1199
Ilia Reingold <ilia.reingold@gmail.com>
2023-07-03 22:48:10 -0400

    fix content uri

--- android/src/main/java/com/wallpaperai/wallpaperhelper/WallpaperHelper.java
+++ android/src/main/java/com/wallpaperai/wallpaperhelper/WallpaperHelper.java
@@ -9,10 +9,12 @@ import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.util.Base64;
 import android.util.Log;
+import androidx.core.content.FileProvider;
 
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
+
 
 public class WallpaperHelper {
 
@@ -21,21 +23,22 @@ public class WallpaperHelper {
         Bitmap bitmap = BitmapFactory.decodeByteArray(decodedString, 0, decodedString.length);
 
         // Convert bitmap to file and get Uri
-        File file = new File(activity.getCacheDir(), "temp.jpg");
+        File file = new File(activity.getCacheDir(), "temp.png");
         try (FileOutputStream out = new FileOutputStream(file)) {
-            bitmap.compress(Bitmap.CompressFormat.JPEG, 100, out);
+            bitmap.compress(Bitmap.CompressFormat.PNG, 100, out);
         } catch (IOException e) {
             Log.e("WallpaperHelper", "Error writing file: " + e.getMessage());
             return;
         }
-        Uri uri = Uri.fromFile(file);
+        Uri uri = FileProvider.getUriForFile(activity, activity.getApplicationContext().getPackageName() + ".fileprovider", file);
 
         // Start the wallpaper setting activity
         WallpaperManager wallpaperManager = WallpaperManager.getInstance(activity);
-        Intent intent;
-        intent = wallpaperManager.getCropAndSetWallpaperIntent(uri);
+        Intent intent = wallpaperManager.getCropAndSetWallpaperIntent(uri);
+        intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION); // Important!
         activity.startActivityForResult(intent, REQUEST_SET_WALLPAPER);
     }
+
 
     // Call this method in your activity's onActivityResult method
     public void handleActivityResult(int requestCode, int resultCode, Intent data) {
