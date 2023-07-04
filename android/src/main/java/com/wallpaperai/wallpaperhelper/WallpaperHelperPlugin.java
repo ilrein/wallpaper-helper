60f13c99c8cc1a18f5062fdf5c4b94997d5b1918
Ilia Reingold <ilia.reingold@gmail.com>
2023-07-04 10:30:23 -0400

    update import for plugin

--- android/src/main/java/com/wallpaperai/wallpaperhelper/WallpaperHelperPlugin.java
+++ android/src/main/java/com/wallpaperai/wallpaperhelper/WallpaperHelperPlugin.java
@@ -7,6 +7,7 @@ import com.getcapacitor.PluginMethod;
 import com.getcapacitor.annotation.CapacitorPlugin;
 import android.content.Intent;
 import android.app.Activity;
+import androidx.core.content.FileProvider; // Add this import statement
 
 @CapacitorPlugin(name = "WallpaperHelper")
 public class WallpaperHelperPlugin extends Plugin {
@@ -20,7 +21,7 @@ public class WallpaperHelperPlugin extends Plugin {
 
         // Save the call so we can resolve it in handleOnActivityResult
         savedCall = call;
-        
+
         try {
             implementation.setWallpaper(getActivity(), base64Image);
         } catch (Exception e) {
@@ -31,11 +32,11 @@ public class WallpaperHelperPlugin extends Plugin {
     @Override
     protected void handleOnActivityResult(int requestCode, int resultCode, Intent data) {
         super.handleOnActivityResult(requestCode, resultCode, data);
-        
+
         if (savedCall == null) {
             return;
         }
-        
+
         JSObject ret = new JSObject();
 
         if (requestCode == WallpaperHelper.REQUEST_SET_WALLPAPER) {
