d686f67d3f68dee891a7fc5591ee83b137985359
Ilia Reingold <ilia.reingold@gmail.com>
2023-07-05 16:46:22 -0400

    try new version

--- android/src/main/java/com/wallpaperai/wallpaperhelper/WallpaperHelper.java
+++ android/src/main/java/com/wallpaperai/wallpaperhelper/WallpaperHelper.java
@@ -3,62 +3,45 @@ package com.wallpaperai.wallpaperhelper;
 import android.app.Activity;
 import android.app.WallpaperManager;
 import android.content.Context;
-import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
-import android.net.Uri;
 import android.util.Base64;
+import android.util.DisplayMetrics;
 import android.util.Log;
-
-import androidx.core.content.FileProvider;
-
-import com.yalantis.ucrop.UCrop;
-
-import java.io.File;
-import java.io.FileOutputStream;
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 
 public class WallpaperHelper {
 
-    public void setWallpaper(Activity activity, String base64Image) {
+    public String setWallpaper(Context context, String base64Image) {
         byte[] decodedString = Base64.decode(base64Image, Base64.DEFAULT);
-        Bitmap bitmap = BitmapFactory.decodeByteArray(decodedString, 0, decodedString.length);
-
-        // Convert bitmap to file and get Uri
-        File cachePath = new File(activity.getCacheDir(), "images");
-        cachePath.mkdirs();
-        File filePath = new File(cachePath, "image.png");
-        try (FileOutputStream out = new FileOutputStream(filePath)) {
-            bitmap.compress(Bitmap.CompressFormat.JPEG, 100, out);
-        } catch (IOException e) {
-            Log.e("WallpaperHelper", "Error writing file: " + e.getMessage());
-            return;
-        }
-        String authority = activity.getApplicationContext().getPackageName() + ".fileprovider";
-        Uri sourceUri = FileProvider.getUriForFile(activity, authority, filePath);
-
-        // Start the cropping activity with uCrop
-        UCrop.of(sourceUri, sourceUri)
-            .start(activity);
-    }
-
-    // Call this method in your activity's onActivityResult method
-    public void handleActivityResult(Activity activity, int requestCode, int resultCode, Intent data) {
-        Log.v("handleOnActivityResult WallpaperHelper", "requestCode: " + requestCode + ", resultCode: " + resultCode);
-        if (requestCode == UCrop.REQUEST_CROP && resultCode == Activity.RESULT_OK) {
-            Uri resultUri = UCrop.getOutput(data);
-
-            // Set the cropped image as wallpaper
-            WallpaperManager wallpaperManager = WallpaperManager.getInstance(activity);
-            try (InputStream in = activity.getContentResolver().openInputStream(resultUri)) {
-                if (in != null) {
-                    wallpaperManager.setStream(in);
-                    Log.v("WallpaperHelper", "Wallpaper set successfully");
-                }
-            } catch (IOException e) {
-                Log.e("WallpaperHelper", "Error setting wallpaper: " + e.getMessage());
+        BitmapFactory.Options options = new BitmapFactory.Options();
+        options.inJustDecodeBounds = true;
+        BitmapFactory.decodeByteArray(decodedString, 0, decodedString.length, options);
+        int imageHeight = options.outHeight;
+        int imageWidth = options.outWidth;
+        int inSampleSize = 1;
+        DisplayMetrics displayMetrics = context.getResources().getDisplayMetrics();
+        int reqHeight = displayMetrics.heightPixels;
+        int reqWidth = displayMetrics.widthPixels;
+        if (imageHeight > reqHeight || imageWidth > reqWidth) {
+            final int halfHeight = imageHeight / 2;
+            final int halfWidth = imageWidth / 2;
+            while ((halfHeight / inSampleSize) >= reqHeight && (halfWidth / inSampleSize) >= reqWidth) {
+                inSampleSize *= 2;
             }
+        }
+        BitmapFactory.Options bitmapOptions = new BitmapFactory.Options();
+        bitmapOptions.inSampleSize = inSampleSize;
+        Bitmap bitmap = BitmapFactory.decodeByteArray(decodedString, 0, decodedString.length, bitmapOptions);
+
+        WallpaperManager wallpaperManager = WallpaperManager.getInstance(context);
+        try {
+            wallpaperManager.setBitmap(bitmap);
+            return "Success";
+        } catch (IOException e) {
+            return "Error setting wallpaper: " + e.getMessage();
         }
     }
 }
--- android/src/main/java/com/wallpaperai/wallpaperhelper/WallpaperHelperPlugin.java
+++ android/src/main/java/com/wallpaperai/wallpaperhelper/WallpaperHelperPlugin.java
@@ -1,20 +1,16 @@
 package com.wallpaperai.wallpaperhelper;
 
+import android.util.Log;
 import com.getcapacitor.JSObject;
 import com.getcapacitor.Plugin;
 import com.getcapacitor.PluginCall;
 import com.getcapacitor.PluginMethod;
 import com.getcapacitor.annotation.CapacitorPlugin;
-import android.content.Intent;
-import android.app.Activity;
-import com.yalantis.ucrop.UCrop;
-import android.util.Log;
 
 @CapacitorPlugin(name = "WallpaperHelper")
 public class WallpaperHelperPlugin extends Plugin {
 
     private WallpaperHelper implementation = new WallpaperHelper();
-    private PluginCall savedCall;
 
     public WallpaperHelperPlugin() {
         super();
@@ -24,43 +20,15 @@ public class WallpaperHelperPlugin extends Plugin {
     @PluginMethod
     public void setWallpaper(PluginCall call) {
         String base64Image = call.getString("base64");
+        boolean setBoth = call.getBoolean("setBoth", false);
 
-        // Save the call so we can resolve it in handleOnActivityResult
-        savedCall = call;
-        
         try {
-            implementation.setWallpaper(getActivity(), base64Image);
+            String result = implementation.setWallpaper(getContext(), base64Image);
+            JSObject ret = new JSObject();
+            ret.put("value", result);
+            call.resolve(ret);
         } catch (Exception e) {
             call.reject(e.getMessage(), e);
-        }
-    }
-
-    @Override
-    protected void handleOnActivityResult(int requestCode, int resultCode, Intent data) {
-        super.handleOnActivityResult(requestCode, resultCode, data);
-
-        Log.v("handleOnActivityResult Plugin", "requestCode: " + requestCode + ", resultCode: " + resultCode);
-
-        if (requestCode == UCrop.REQUEST_CROP) {
-            Log.v("handleOnActivityResult Plugin", "Request code matches UCrop.REQUEST_CROP");
-        } else {
-            Log.v("handleOnActivityResult Plugin", "Request code does not match UCrop.REQUEST_CROP");
-        }
-        
-        if (savedCall == null) {
-            return;
-        }
-        
-        JSObject ret = new JSObject();
-
-        if (requestCode == UCrop.REQUEST_CROP) {
-            if (resultCode == Activity.RESULT_OK) {
-                ret.put("value", "Success");
-                savedCall.resolve(ret);
-            } else {
-                savedCall.reject("Cancelled");
-            }
-            savedCall = null;
         }
     }
 }
