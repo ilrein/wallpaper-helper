b4b8021ab54dc263bffed3b50a41dc971ee78dc4
Ilia Reingold <ilia.reingold@gmail.com>
2023-07-15 10:08:09 -0400

    back to ucrop 0.0.28

--- android/src/main/java/com/wallpaperai/wallpaperhelper/WallpaperHelper.java
+++ android/src/main/java/com/wallpaperai/wallpaperhelper/WallpaperHelper.java
@@ -8,7 +8,14 @@ import android.graphics.BitmapFactory;
 import android.util.Base64;
 import android.util.DisplayMetrics;
 import android.util.Log;
+<<<<<<< Updated upstream
 import java.io.ByteArrayInputStream;
+=======
+import androidx.core.content.FileProvider;
+import com.yalantis.ucrop.UCrop;
+import java.io.File;
+import java.io.FileOutputStream;
+>>>>>>> Stashed changes
 import java.io.IOException;
 import java.io.InputStream;
 
@@ -16,20 +23,40 @@ public class WallpaperHelper {
 
     public String setWallpaper(Context context, String base64Image) {
         byte[] decodedString = Base64.decode(base64Image, Base64.DEFAULT);
-        BitmapFactory.Options options = new BitmapFactory.Options();
-        options.inJustDecodeBounds = true;
-        BitmapFactory.decodeByteArray(decodedString, 0, decodedString.length, options);
-        int imageHeight = options.outHeight;
-        int imageWidth = options.outWidth;
-        int inSampleSize = 1;
-        DisplayMetrics displayMetrics = context.getResources().getDisplayMetrics();
-        int reqHeight = displayMetrics.heightPixels;
-        int reqWidth = displayMetrics.widthPixels;
-        if (imageHeight > reqHeight || imageWidth > reqWidth) {
-            final int halfHeight = imageHeight / 2;
-            final int halfWidth = imageWidth / 2;
-            while ((halfHeight / inSampleSize) >= reqHeight && (halfWidth / inSampleSize) >= reqWidth) {
-                inSampleSize *= 2;
+        Bitmap bitmap = BitmapFactory.decodeByteArray(decodedString, 0, decodedString.length);
+
+        // Convert bitmap to file and get Uri
+        File cachePath = new File(activity.getCacheDir(), "images");
+        cachePath.mkdirs();
+        File filePath = new File(cachePath, "image.png");
+        try (FileOutputStream out = new FileOutputStream(filePath)) {
+            bitmap.compress(Bitmap.CompressFormat.PNG, 100, out);
+        } catch (IOException e) {
+            Log.e("WallpaperHelper", "Error writing file: " + e.getMessage());
+            return;
+        }
+        String authority = activity.getApplicationContext().getPackageName() + ".fileprovider";
+        Uri sourceUri = FileProvider.getUriForFile(activity, authority, filePath);
+
+        // Start the cropping activity with uCrop
+        UCrop.of(sourceUri, sourceUri).start(activity);
+    }
+
+    // Call this method in your activity's onActivityResult method
+    public void handleActivityResult(Activity activity, int requestCode, int resultCode, Intent data) {
+        Log.v("handleOnActivityResult WallpaperHelper", "requestCode: " + requestCode + ", resultCode: " + resultCode);
+        if (requestCode == UCrop.REQUEST_CROP && resultCode == Activity.RESULT_OK) {
+            Uri resultUri = UCrop.getOutput(data);
+
+            // Set the cropped image as wallpaper
+            WallpaperManager wallpaperManager = WallpaperManager.getInstance(activity);
+            try (InputStream in = activity.getContentResolver().openInputStream(resultUri)) {
+                if (in != null) {
+                    wallpaperManager.setStream(in);
+                    Log.v("WallpaperHelper", "Wallpaper set successfully");
+                }
+            } catch (IOException e) {
+                Log.e("WallpaperHelper", "Error setting wallpaper: " + e.getMessage());
             }
         }
         BitmapFactory.Options bitmapOptions = new BitmapFactory.Options();
