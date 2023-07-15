5a946cef8ebcfbc9f1f445fe36f2d2fc281ce132
Ilia Reingold <ilia.reingold@gmail.com>
2023-07-15 11:48:16 -0400

    try new form 35

--- android/src/main/java/com/wallpaperai/wallpaperhelper/WallpaperHelperPlugin.java
+++ android/src/main/java/com/wallpaperai/wallpaperhelper/WallpaperHelperPlugin.java
@@ -26,45 +26,29 @@ public class WallpaperHelperPlugin extends Plugin {
     public void setWallpaper(PluginCall call) {
         String base64Image = call.getString("base64");
         boolean setBoth = call.getBoolean("setBoth", false);
-        Log.v("WallpaperHelperPlugin", "setWallpaper");
 
         try {
             byte[] decodedString = Base64.decode(base64Image, Base64.DEFAULT);
             Bitmap bitmap = BitmapFactory.decodeByteArray(decodedString, 0, decodedString.length);
             implementation.startCroppingActivity(getContext(), bitmap);
 
-            Log.v("WallpaperHelperPlugin", "saveCall");
-
+            // Save the call for later
             saveCall(call);
         } catch (Exception e) {
-            Log.e("WallpaperHelperPlugin", "Error setting wallpaper: " + e.getMessage());
             call.reject(e.getMessage(), e);
         }
     }
 
-    @Override
-    protected void handleOnActivityResult(int requestCode, int resultCode, Intent data) {
-        super.handleOnActivityResult(requestCode, resultCode, data);
-
-        PluginCall savedCall = getSavedCall();
-        if (savedCall == null) {
-            Log.e("WallpaperHelperPlugin", "No stored plugin call for onActivityResult");
+    @ActivityCallback
+    private void handleActivityResult(PluginCall call, ActivityResult result) {
+        if (call == null) {
             return;
         }
 
-        Log.v("WallpaperHelperPlugin", "onActivityResult");
-        Log.v("WallpaperHelperPlugin", String.valueOf(requestCode));
-        Log.v("WallpaperHelperPlugin", String.valueOf(resultCode));
-        Log.v("WallpaperHelperPlugin", String.valueOf(CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE));
-
-        if (requestCode == CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE) {
-            boolean setBoth = savedCall.getBoolean("setBoth", false);
-            implementation.handleActivityResult(getActivity(), requestCode, resultCode, data, setBoth);
-            JSObject ret = new JSObject();
-            ret.put("value", "Success");
-            savedCall.resolve(ret);
-        } else {
-            savedCall.reject("Failed to set wallpaper");
-        }
+        boolean setBoth = call.getBoolean("setBoth", false);
+        implementation.handleActivityResult(getActivity(), result.getResultCode(), result.getData(), setBoth);
+        JSObject ret = new JSObject();
+        ret.put("value", "Success");
+        call.resolve(ret);
     }
 }
