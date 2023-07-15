824c8ea1da5035c34a6583ebedd4e8f6dc2cb0ac
Ilia Reingold <ilia.reingold@gmail.com>
2023-07-15 10:21:37 -0400

    v30

--- android/src/main/java/com/wallpaperai/wallpaperhelper/WallpaperHelper.java
+++ android/src/main/java/com/wallpaperai/wallpaperhelper/WallpaperHelper.java
@@ -3,10 +3,11 @@ package com.wallpaperai.wallpaperhelper;
 import android.app.Activity;
 import android.app.WallpaperManager;
 import android.content.Context;
+import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.util.Base64;
-import android.util.DisplayMetrics;
 import android.util.Log;
 import androidx.core.content.FileProvider;
 import com.yalantis.ucrop.UCrop;
@@ -17,12 +18,9 @@ import java.io.InputStream;
 
 public class WallpaperHelper {
 
-    public String setWallpaper(Context context, String base64Image) {
-        byte[] decodedString = Base64.decode(base64Image, Base64.DEFAULT);
-        Bitmap bitmap = BitmapFactory.decodeByteArray(decodedString, 0, decodedString.length);
-
+    public void startCroppingActivity(Context context, Bitmap bitmap) {
         // Convert bitmap to file and get Uri
-        File cachePath = new File(activity.getCacheDir(), "images");
+        File cachePath = new File(context.getCacheDir(), "images");
         cachePath.mkdirs();
         File filePath = new File(cachePath, "image.png");
         try (FileOutputStream out = new FileOutputStream(filePath)) {
@@ -31,16 +29,14 @@ public class WallpaperHelper {
             Log.e("WallpaperHelper", "Error writing file: " + e.getMessage());
             return;
         }
-        String authority = activity.getApplicationContext().getPackageName() + ".fileprovider";
-        Uri sourceUri = FileProvider.getUriForFile(activity, authority, filePath);
+        String authority = context.getApplicationContext().getPackageName() + ".fileprovider";
+        Uri sourceUri = FileProvider.getUriForFile(context, authority, filePath);
 
         // Start the cropping activity with uCrop
-        UCrop.of(sourceUri, sourceUri).start(activity);
+        UCrop.of(sourceUri, sourceUri).start((Activity) context);
     }
 
-    // Call this method in your activity's onActivityResult method
     public void handleActivityResult(Activity activity, int requestCode, int resultCode, Intent data) {
-        Log.v("handleOnActivityResult WallpaperHelper", "requestCode: " + requestCode + ", resultCode: " + resultCode);
         if (requestCode == UCrop.REQUEST_CROP && resultCode == Activity.RESULT_OK) {
             Uri resultUri = UCrop.getOutput(data);
 
@@ -54,17 +50,6 @@ public class WallpaperHelper {
             } catch (IOException e) {
                 Log.e("WallpaperHelper", "Error setting wallpaper: " + e.getMessage());
             }
-        }
-        BitmapFactory.Options bitmapOptions = new BitmapFactory.Options();
-        bitmapOptions.inSampleSize = inSampleSize;
-        Bitmap bitmap = BitmapFactory.decodeByteArray(decodedString, 0, decodedString.length, bitmapOptions);
-
-        WallpaperManager wallpaperManager = WallpaperManager.getInstance(context);
-        try {
-            wallpaperManager.setBitmap(bitmap);
-            return "Success";
-        } catch (IOException e) {
-            return "Error setting wallpaper: " + e.getMessage();
         }
     }
 }
--- android/src/main/java/com/wallpaperai/wallpaperhelper/WallpaperHelperPlugin.java
+++ android/src/main/java/com/wallpaperai/wallpaperhelper/WallpaperHelperPlugin.java
@@ -1,11 +1,16 @@
 package com.wallpaperai.wallpaperhelper;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.util.Base64;
 import android.util.Log;
 import com.getcapacitor.JSObject;
 import com.getcapacitor.Plugin;
 import com.getcapacitor.PluginCall;
 import com.getcapacitor.PluginMethod;
 import com.getcapacitor.annotation.CapacitorPlugin;
+import com.yalantis.ucrop.UCrop;
 
 @CapacitorPlugin(name = "WallpaperHelper")
 public class WallpaperHelperPlugin extends Plugin {
@@ -23,12 +28,33 @@ public class WallpaperHelperPlugin extends Plugin {
         boolean setBoth = call.getBoolean("setBoth", false);
 
         try {
-            String result = implementation.setWallpaper(getContext(), base64Image);
-            JSObject ret = new JSObject();
-            ret.put("value", result);
-            call.resolve(ret);
+            byte[] decodedString = Base64.decode(base64Image, Base64.DEFAULT);
+            Bitmap bitmap = BitmapFactory.decodeByteArray(decodedString, 0, decodedString.length);
+            implementation.startCroppingActivity(getContext(), bitmap);
+
+            saveCall(call);
         } catch (Exception e) {
             call.reject(e.getMessage(), e);
+        }
+    }
+
+    @Override
+    protected void handleOnActivityResult(int requestCode, int resultCode, Intent data) {
+        super.handleOnActivityResult(requestCode, resultCode, data);
+
+        PluginCall savedCall = getSavedCall();
+        if (savedCall == null) {
+            Log.e("WallpaperHelperPlugin", "No stored plugin call for onActivityResult");
+            return;
+        }
+
+        if (requestCode == UCrop.REQUEST_CROP && resultCode == getActivity().RESULT_OK) {
+            implementation.handleActivityResult(getActivity(), requestCode, resultCode, data);
+            JSObject ret = new JSObject();
+            ret.put("value", "Success");
+            savedCall.resolve(ret);
+        } else {
+            savedCall.reject("Failed to set wallpaper");
         }
     }
 }
